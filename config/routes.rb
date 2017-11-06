35c95e3ffe233cb77dca27357d4755d6c7371c80
snayrouz <samuel.nayrouz@gmail.com>
2017-11-06 10:26:46 -0700

    Remove all devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "home#index"
-
-  get '/auth/twitter', to: 'sessions#create', as: 'twitter_login'
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
 
   get '/login', :to => 'sessions#new', :as => 'login'
   post '/login', :to => 'sessions#create'
