3f1f5214e3e9dd5e0f374cdfb46ea003ff5ae633
snayrouz <samuel.nayrouz@gmail.com>
2017-11-06 22:19:17 -0700

    Controller tests pass

--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Currency, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:date) }
+  it { should validate_presence_of(:currency_type) }
+
+  it 'should throw error for invalid value of currency_type' do
+    expect { build(:currency, currency_type: "invalid") }.to raise_error(ArgumentError).with_message(/is not a valid currency_type/)
+  end
 end
