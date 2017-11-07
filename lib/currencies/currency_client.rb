df4c297022df4a426b47258f3e7c16f58e19f532
snayrouz <samuel.nayrouz@gmail.com>
2017-11-06 21:44:51 -0700

    Currency client works inpostman

--- lib/currencies/currency_client.rb
+++ lib/currencies/currency_client.rb
@@ -1,14 +1,21 @@
 module CurrencyClient
   def self.get_bitcoins
-
+    get_currencies(APP_CONFIG.fetch('bitcoin_api_url'))
   end
 
   def self.get_ethereums
-
+    get_currencies(APP_CONFIG.fetch('ethereum_api_url'))
   end
 
   def self.get_nasdaqs
-
+    json = get_json_response(APP_CONFIG.fetch('nasdaq_api_url'))
+    values = json['seriess'].first['obs']
+    values.inject({}) do |new_element, current_element|
+      key = DateTime.strptime((current_element[0] / 1000).to_s, '%s')
+      value = current_element[1]
+      new_element[key] = value
+      new_element
+    end
   end
 
     private
