07bb0b99da0bb0b3f0f329c91c11dd6e0f55c456
snayrouz <samuel.nayrouz@gmail.com>
2017-11-06 21:39:49 -0700

    Add json response method

--- lib/currencies/currency_client.rb
+++ lib/currencies/currency_client.rb
@@ -14,7 +14,19 @@ module CurrencyClient
     private
 
     def self.get_currencies(api_url)
-
+      json_response = get_json_response(api_url)
+      json_response['Data'].inject({}) do |new_element, current_element|
+        key = DateTime.strptime(current_element['time'].to_s, '%s')
+        value = current_element['close']
+        new_element[key] = value
+        new_element
+      end
     end
-  
+
+    def self.get_json_response(api_url)
+      response = RestClient.get(api_url)
+      json = JSON.parse(response)
+      json
+    end
+
 end
