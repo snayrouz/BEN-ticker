880c3ed6e3e085348bcf013b78738b7d8c247190
snayrouz <samuel.nayrouz@gmail.com>
2017-11-06 21:23:16 -0700

    Add method skeletons to currency client

--- lib/currencies/currency_client.rb
+++ lib/currencies/currency_client.rb
@@ -1,4 +1,20 @@
 module CurrencyClient
+  def self.get_bitcoins
 
+  end
 
+  def self.get_ethereums
+
+  end
+
+  def self.get_nasdaqs
+
+  end
+
+    private
+
+    def self.get_currencies(api_url)
+
+    end
+  
 end
