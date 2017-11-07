0328c6f49194be2123894f31a933c1d9b71416c9
snayrouz <samuel.nayrouz@gmail.com>
2017-11-06 20:44:34 -0700

    Setup dashboard show rspec test and delete unnecessary tests of visitors

--- spec/features/visitor/visitor_cant_view_dashboard_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe "Visitor Can Not View Dashboard" do
-  it "visits dashboard path and fails" do
-    visit 'dashboard_index_path'
-
-    expect(response).to have_http_status(404)
-  end
-end
