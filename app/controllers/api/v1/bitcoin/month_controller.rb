42c1b0c8a321bda606cb7a28f538a360a2db9612
snayrouz <samuel.nayrouz@gmail.com>
2017-11-08 09:36:00 -0700

    Add view for bitcoin-god driven development

--- app/controllers/api/v1/bitcoin/month_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::Bitcoin:MonthController < ApplicationController
-  def show
-    render json: Bitcoin.sort_by_month
-  end
-end
--- app/controllers/api/v1/bitcoin/week_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::Bitcoin:WeekController < ApplicationController
-  def show
-    render json: Bitcoin.sort_by_week
-  end
-end
