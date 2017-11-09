7a12003cecb6d0bf51c7b9669727c499ed9b384c
snayrouz <samuel.nayrouz@gmail.com>
2017-11-08 18:03:01 -0700

    Change config streaming to rest

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -15,7 +15,7 @@ class TwitterApi
   end
 
   def self.client
-    @client ||= Twitter::Streaming::Client.new do |config|
+    @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
       config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
