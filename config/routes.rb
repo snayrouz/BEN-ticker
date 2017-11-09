85f705a9446817345f4d98127f55662c3505add9
snayrouz <samuel.nayrouz@gmail.com>
2017-11-09 15:56:17 -0700

    Add token cntroller

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
 
   get '/login', :to => 'sessions#new', :as => 'login'
   post '/login', :to => 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   get '/password-reset', to: 'passwords#new', as: 'new_password_reset'
   post '/password-reset', to: 'passwords#create', as: 'password_reset'
 
   get '/password-confirmation', to: 'confirmations#new', as: 'new_password'
   post '/password-confirmation', to: 'passwords#update', as: 'edit_password'
+
+  post 'tokens' => 'tokens#create'
 
   resources :users, only: [:new, :create, :edit, :update]
 
